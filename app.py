9729f2c5c0500044ddf54928be6ff4fb40773fab
Tomas Coufal <tcoufal@redhat.com>
2018-10-08 16:33:08 +0200

    Make code PEP complient

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import os
-from time import sleep
 import logging
 from kafka import KafkaConsumer
 
@@ -8,24 +7,25 @@ logging.basicConfig(level=logging.WARNING)
 logger = logging.getLogger('consumer')
 logger.setLevel(logging.INFO)
 
-def consumer(server, topic):
-    '''
-    Simple Kafka consumer
+
+def listen(server: str, topic: str) -> None:
+    """Kafka messages listener.
 
     Connects to Kafka server, consumes a topic and yields every message it
     receives.
-    '''
-    logger.info(f'Connecting to Kafka server ({server})')
-    logger.info(f'Subscribing to topic: "{topic}"')
+    """
+    logger.info('Connecting to Kafka server (%s)', server)
+    logger.info('Subscribing to topic: "%s"', topic)
     consumer = KafkaConsumer(topic, bootstrap_servers=server)
 
+    # pylama:ignore=W0106
     [logger.info(msg) for msg in consumer]
 
 
 if __name__ == '__main__':
     # Env. variables passed to container
-    kafka_server = os.environ.get('KAFKA_SERVER')
-    topic = os.environ.get('TOPIC')
+    KAFKA_SERVER = os.environ.get('KAFKA_SERVER')
+    KAFKA_TOPIC = os.environ.get('TOPIC')
 
     # Run the consumer
-    consumer(kafka_server, topic)
+    listen(KAFKA_SERVER, KAFKA_TOPIC)
