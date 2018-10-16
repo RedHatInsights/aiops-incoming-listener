aa41dce2cc9f734ff273777eabe35584b2ba27ac
Tomas Coufal <tcoufal@redhat.com>
2018-10-16 13:56:04 +0200

    Convert NEXT_MICROSERVICE_URL to NEXT_MICROSERVICE_HOST and check env variables

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import logging
+import sys
 from json import loads
 
 from kafka import KafkaConsumer
@@ -11,26 +12,32 @@ logger = logging.getLogger('consumer')
 logger.setLevel(logging.INFO)
 
 
-def hit_next_in_pipepine(url: str, payload: dict) -> None:
+def hit_next_in_pipepine(payload: dict) -> None:
     """Pass the data to next service in line."""
-    # Do not wait for response now, so we can keep listening
+    host = os.environ.get('NEXT_MICROSERVICE_HOST')
+
     # FIXME: Convert to aiohttp or some other async requests alternative
+    # Do not wait for response now, so we can keep listening
     try:
-        requests.post(url, json=payload, timeout=10)
+        requests.post(f'http://{host}', json=payload, timeout=10)
     except requests.exceptions.ReadTimeout:
         pass
     except requests.exceptions.ConnectionError as e:
         logger.warning('Call to next service failed: %s', str(e))
 
 
-def listen(server: str, topic: str) -> dict:
+def listen() -> dict:
     """Kafka messages listener.
 
     Connects to Kafka server, consumes a topic and yields every message it
     receives.
     """
+    server = os.environ.get('KAFKA_SERVER')
+    topic = os.environ.get('KAFKA_TOPIC')
+
     logger.info('Connecting to Kafka server (%s)', server)
     logger.info('Subscribing to topic: "%s"', topic)
+
     consumer = KafkaConsumer(topic, bootstrap_servers=server)
 
     for msg in consumer:
@@ -39,13 +46,19 @@ def listen(server: str, topic: str) -> dict:
 
 
 if __name__ == '__main__':
-    # Env. variables passed to container
-    KAFKA_SERVER = os.environ.get('KAFKA_SERVER')
-    KAFKA_TOPIC = os.environ.get('KAFKA_TOPIC')
-    NEXT_MICROSERVICE_URL = os.environ.get('NEXT_MICROSERVICE_URL')
+    # Check environment variables passed to container
+    # pylama:ignore=C0103
+    env = {'KAFKA_SERVER', 'KAFKA_TOPIC', 'NEXT_MICROSERVICE_HOST'}
+
+    if not env.issubset(os.environ):
+        logger.error(
+            'Environment not set properly, missing %s',
+            env - set(os.environ)
+        )
+        sys.exit(1)
 
     # Run the consumer
-    for received in listen(KAFKA_SERVER, KAFKA_TOPIC):
+    for received in listen():
         if 'url' not in received:
             logger.warning(
                 'Message is missing data location URL. Message ignored: %s',
@@ -61,4 +74,4 @@ if __name__ == '__main__':
             }
         }
 
-        hit_next_in_pipepine(NEXT_MICROSERVICE_URL, message)
+        hit_next_in_pipepine(message)
